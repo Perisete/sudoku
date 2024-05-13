e04297a4f7939637666bfe157d34a0974a522130
Jorge Peris <peris993@gmail.com>
2024-05-13 10:42:15 +0200

    update

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define SUDOKU_SIZE 9
+
+struct allowed_struct{
+    int size;
+    int *array;
+};
+
 int **allocate_board(int rows, int cols)
 {
     int **board = (int **) malloc(rows * sizeof(int *));
@@ -13,7 +20,30 @@ int **allocate_board(int rows, int cols)
     return board;
 }
 
-int **zeroes_board(int **board, int rows, int cols)
+void free_board(int **board, int rows)
+{
+    for(int i=0;i<rows;i++)
+    {
+        free(board[i]);
+    }
+}
+
+void print_array(int *arr, int size)
+{
+    int i;
+    printf("[");
+    for(i=0;i<size;i++)
+    {
+        printf("%d", arr[i]);
+        if(i < size - 1)
+        {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+int **zeroes_board(int **board, int rows, int cols) // Needed?
 {
     int i = 0, j = 0;
     for(i=0;i<rows;i++)
@@ -26,71 +56,207 @@ int **zeroes_board(int **board, int rows, int cols)
     return board;
 }
 
-int check_if_allowed_square(int **board, int rows, int cols, int row_start, int row_end, int col_start, int col_end, int num)
-{
-    int row, col;
-    printf("row start: %d\n", row_start);
-    printf("row end: %d\n", row_end);
-    printf("col start: %d\n", col_start);
-    printf("col end: %d\n", col_end);
-    for(row=row_start;row<row_end;row++)
-    {
-        for(col=col_start;col<col_end;col++)
-        {
-            if(board[row][col] == num)
-            {
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
-int **get_values(int **board, int rows, int cols)
-{
-    int row = 0, col = 0, num = 0, allowed = 1, col_mult = 0, row_mult = 0;
-    srand(time(NULL));
-    for(row=0;row<rows;row++)
-    {
-        col_mult = 0;
-        for(col=0;col<cols;col++)
-        {
-            if(col % 3 == 0 && col > 0)
-            { 
-                col_mult++;
-            }
-            do{
-                num = (rand() % 9) + 1;
-                allowed = check_if_allowed_square(board, rows/3, cols/3, row - row_mult, row - (row_mult*3), col - (col_mult*3), row - (col_mult*3), num);
-            }while(allowed != 1);
-            board[row][col] = num;
-        }
-        if(row % 3 == 0 && row > 0)
-        {
-            row_mult++;
-        }
-    }
-    return board;
-}
-
 void draw_board(int **board, int rows, int cols){
     int i = 0, j = 0;
     for(i;i<rows;i++)
     {
-        for(j=0;j<cols;j++){
+        for(j=0;j<cols;j++)
+        {
             printf("%d ", board[i][j]);
         }
         printf("\n");
     }
 }
 
+struct allowed_struct init_struct(int s)
+{
+    struct allowed_struct as;
+    as.size = s;
+    as.array = (int *) malloc(s * sizeof(int));
+    return as;
+}
+
+struct allowed_struct check_row(int **board, struct allowed_struct as, int row, int cols)
+{
+    // int *temp = (int *) malloc(cols * sizeof(int));
+    int *temp = as.array;
+    int pos = 0;
+    for(int i=1;i<=cols;i++)
+    {
+        for(int col=0;col<cols;col++)
+        {
+            if(i == board[row][col])
+            {
+                    break;
+            }
+            if(col == cols - 1)
+            {
+                temp[pos] = i;
+                pos++;
+            }
+        }
+    }
+    as.size = pos;
+    as.array = (int *) realloc(temp, pos * sizeof(int));
+    if(temp == NULL)
+    {
+        printf("Memory reallocation failed!\n");
+        free(temp);
+        // return 1;
+    }
+    temp = as.array;
+    return as;
+}
+
+struct allowed_struct check_col(int **board, struct allowed_struct allowed_rows, int rows, int col)
+{
+    // int *temp = (int *) malloc(allowed_rows.size * sizeof(int));
+    int *temp = allowed_rows.array;
+    int pos = 0, found = 0;
+    for(int i=0;i<allowed_rows.size;i++)
+    {
+        for(int row=0;row<rows;row++)
+        {
+            if(allowed_rows.array[i] == board[row][col])
+            {
+                    break;
+            }
+            if(row == rows - 1)
+            {
+                temp[pos] = allowed_rows.array[i];
+                pos++;
+            }
+        }
+    }
+    int *res = (int *) realloc(temp, pos * sizeof(int));
+    struct allowed_struct as;
+    as.size = pos;
+    as.array = res;
+    return as;
+}
+
+struct allowed_struct check_segment(int **board, struct allowed_struct allowed_arr, int start_row, int start_col)
+{
+    // int *temp = (int *) malloc(allowed_arr.size * sizeof(int));
+    int *temp = allowed_arr.array;
+    int pos = 0, found, row, col;
+    for(int i=0;i<allowed_arr.size;i++)
+    {
+        found = 0;
+        for(row=start_row;row<start_row+3;row++)
+        {
+            for(col=start_col;col<start_col+3;col++)
+            {
+                if(allowed_arr.array[i] == board[row][col])
+                {
+                        found = 1;
+                        break;
+                }
+            }
+            if(found == 1)
+            {
+                break;
+            }
+        }
+        if(found == 0)
+        {
+            temp[pos] = allowed_arr.array[i];
+            pos++;
+        }
+    }
+    int *res = (int *) realloc(temp, pos * sizeof(int));
+    struct allowed_struct as;
+    as.size = pos;
+    as.array = res;
+    return as;
+}
+
+int **get_values(int **board, int rows, int cols)
+{
+    srand(time(NULL));
+    struct allowed_struct as;
+    int col, row, num, h, v, hseg = 0, vseg = 0;
+    as = init_struct(rows);
+    for(row=0;row<rows;row++)
+    {
+        if(row%3 == 0 && row != 0)
+        {
+            hseg++;
+        }
+        vseg = 0;
+        for(col=0;col<cols;col++)
+        {
+           if(col%3 == 0 && col != 0)
+           {
+               vseg++;
+           }
+           as = check_row(board, as, row, cols);
+           printf("check_row\n");
+           print_array(as.array, as.size);
+           printf("array size1; %d\n", as.size);
+           as = check_col(board, as, rows, col);
+           printf("check_col\n");
+           print_array(as.array, as.size);
+           printf("array size2; %d\n", as.size);
+           for(h=hseg+1;h<rows/3;h++)
+           {
+               as = check_segment(board, as, h*3, vseg*3);
+           }
+           for(v=vseg+1;v<cols/3;v++)
+           {
+               as = check_segment(board, as, hseg*3, v*3);
+           }
+           printf("check_seg\n");
+           print_array(as.array, as.size);
+           printf("array size3; %d\n", as.size);
+           num = rand() % as.size;
+           printf("random: %d\n", num);
+           printf("hseg: %d\nvseg: %d\n",hseg,vseg);
+           board[row][col] = as.array[num];
+           draw_board(board, rows, cols);
+        }
+    }
+    printf("fin\n");
+    return board;
+}
+
 int main()
 {
-    int rows = 9,cols = 9;
+    int rows = SUDOKU_SIZE, cols = rows;
     int **board = allocate_board(rows, cols);
     board = zeroes_board(board, rows, cols);
+    // ########## debug ################
+    /*
+    for(int j=0;j<cols;j++)
+    {
+        if(j == 0)
+        {
+            board[0][j] = 6;
+        }
+        else
+        {
+            board[0][j] = 0;
+        }
+    }
+    
+    board[0][0] = 3;
+    board[0][1] = 7;
+    board[0][2] = 1;
+    // ##########################
+    as = check_row(board, 1, cols);
+    as = check_col(board, as, rows, 0);
+    as = check_segment(board, as, 0, 0);
+    for(int i=0;i<as.size;i++)
+    {
+        printf("%d ", as.array[i]);
+    }
+    printf("\n");
+    board = get_values(board, rows, cols);
+    */
+    // free(allowed_values);
+    // draw_board(board, rows, cols);
     board = get_values(board, rows, cols);
     draw_board(board, rows, cols);
-    free(board); // TODO do it for matrix
+    free_board(board, rows);
     return 0;
 }
